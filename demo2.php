107c8cfe663ca619ffb0eb8bb9de9b8e96a4184a
Harshit Paneri <harshitpanari@gmail.com>
2022-12-06 11:05:23 +0530

    string play

--- demo2.php
+++ demo2.php
@@ -1,11 +1,23 @@
 <?php
 $a = 5;
 $b = 10.5;
-echo $a + $b;
-echo "Value of A";
-var_dump($a);
+// echo $a + $b;
+// echo "Value of A";
+// var_dump($a);
+// echo "<br>";
+// echo "Value of B";
+// var_dump($a);
+// echo "<br>";
+// echo is_int($a);
+// $c;
+// echo $c;
+$class = array("abir","bengali","mamta di");
+// echo $class;
+var_dump($class);
 echo "<br>";
-echo "Value of B";
-var_dump($b);
+echo strlen("Abir Bengali");
 echo "<br>";
+echo str_word_count("Abir Bengali");
+echo "<br>";
+echo strpos("Abir Bengali","Bengali");
 ?>
