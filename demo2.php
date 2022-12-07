849f434740f15ab9b42d6a4434e0ce96cde41b55
Harshit Paneri <harshitpanari@gmail.com>
2022-12-07 09:56:53 +0530

    add more file

--- demo2.php
+++ demo2.php
@@ -11,13 +11,48 @@ $b = 10.5;
 // echo is_int($a);
 // $c;
 // echo $c;
-$class = array("abir","bengali","mamta di");
-// echo $class;
-var_dump($class);
+// $class = array("abir","bengali","mamta di");
+// // echo $class;
+// var_dump($class);
+// echo "<br>";
+// echo strlen("Abir Bengali");
+// echo "<br>";
+// echo str_word_count("Abir Bengali");
+// echo "<br>";
+// echo strpos("Abir Bengali","Bengali");
+// echo "<br>".str_replace("NJR","Udaipur","Techno NJR");
+
+// $str = "Techno India NJR";
+// echo "<br>".strlen($str);
+// echo "<br>".str_word_count($str);
+// echo "<br>".strrev($str);
+// // HW
+// echo strtoupper($x);
+// echo "<br>";
+// echo strtolower($x);
+// echo "<br>";
+// //concat two string
+// echo $x." ".$y;
+// echo "<br>";
+// //find name from email id
+// $email = "harshit@gmail.com";
+// $username=strstr($email,'@',true);
+// echo $username;
+
+$x =4;
+echo is_numeric($x);
 echo "<br>";
-echo strlen("Abir Bengali");
+$y = "789";
+echo var_dump($y);
+// echo is_float($y);
 echo "<br>";
-echo str_word_count("Abir Bengali");
+$l = (int)$y;
+echo var_dump($l);
+
+
+
 echo "<br>";
-echo strpos("Abir Bengali","Bengali");
+$z = 5;
+echo is_int($z);
+
 ?>
---
+++ demo3.php
@@ -0,0 +1,10 @@
+<?php
+
+define("college","Techno India");
+echo college; 
+echo"<br>";
+
+$x = 25;
+$y = 5;
+echo $x/$y;
+?>
